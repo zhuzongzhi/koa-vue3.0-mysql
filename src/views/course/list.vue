d83e3d154064d6dfe66a1a0d1f29c520aa0f5702
windmill <1031199762@qq.com>
2019-02-17 14:25:48 +0800

    course.vue

--- src/views/course/list.vue
+++ src/views/course/list.vue
@@ -46,8 +46,47 @@
         </div>
 
         <!--课程列表-->
-        <div>
+        <div class="list-content">
+            <div>
+                <el-button round @click="isLastest = true" size="mini"
+                           :class="{isActive: isLastest}">最新</el-button>
+                <el-button round @click="isLastest = false" size="mini"
+                           :class="{isActive: !isLastest}">最热</el-button>
+            </div>
 
+            <div class="list-card">
+                <el-row>
+                    <el-col :span="4" v-for="(course, index) in allCourses" :key="index"
+                            :offset="(index > 0 && index%5) ? 1 : 0" style="margin-bottom: 15px;">
+                        <el-card :body-style="{ padding: '0px', height: '260px' }">
+                            <img :src="course.imgUrl" class="image">
+                            <div style="padding: 14px;">
+                                <span>{{course.name}}</span>
+                                <div class="bottom clearfix">
+                                    <time class="time">{{ new Date() }}</time>
+                                    <el-button type="text" class="button">详情</el-button>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                </el-row>
+
+                <el-pagination
+                        style="width: 30%; margin: 0 auto; "
+                        @size-change="handleSizeChange"
+                        @current-change="handleCurrentChange"
+                        :current-page="currentPage4"
+                        :page-sizes="[100, 200, 300, 400]"
+                        :page-size="100"
+                        layout="total, sizes, prev, pager, next, jumper"
+                        :total="400">
+                </el-pagination>
+            </div>
+        </div>
+
+        <div class="footer">
+            <p>Copyright © 2019 imooc.com All Rights Reserved | 京ICP备 12003892号-11</p>
+            <p>如有侵权，请及时联系网站管理员立即删除。</p>
         </div>
     </div>
 </template>
@@ -64,9 +103,92 @@
           choseDirectionIdx: 0,
           choseCategoryIdx: 0,
           choseDifficultIdx: 0,
+          isLastest: true,
+          allCourses: [
+            {
+              name: '社交类App设计入门',
+              briefIntroduction: '综合案例带你详细透析社区产品交互设计',
+              imgUrl: require("../../assets/course/demo1.jpg"),
+              keywords: ['App', '产品交互'],
+              difficultIdx: '初级',
+              hot: 1290,
+            }, {
+              name: '自然语言处理（NLP）入门与实践',
+              briefIntroduction: '一步步教你轻松学习自然语言处理基础与实践',
+              imgUrl: require("../../assets/course/demo2.jpg"),
+              keywords: ['机器学习', '自然语言处理'],
+              difficultIdx: '初级',
+              hot: 2314,
+            },{
+              name: '社交类App设计入门',
+              briefIntroduction: '综合案例带你详细透析社区产品交互设计',
+              imgUrl: require("../../assets/course/demo1.jpg"),
+              keywords: ['App', '产品交互'],
+              difficultIdx: '初级',
+              hot: 1290,
+            }, {
+              name: '自然语言处理（NLP）入门与实践',
+              briefIntroduction: '一步步教你轻松学习自然语言处理基础与实践',
+              imgUrl: require("../../assets/course/demo2.jpg"),
+              keywords: ['机器学习', '自然语言处理'],
+              difficultIdx: '初级',
+              hot: 2314,
+            },{
+              name: '社交类App设计入门',
+              briefIntroduction: '综合案例带你详细透析社区产品交互设计',
+              imgUrl: require("../../assets/course/demo1.jpg"),
+              keywords: ['App', '产品交互'],
+              difficultIdx: '初级',
+              hot: 1290,
+            }, {
+              name: '自然语言处理（NLP）入门与实践',
+              briefIntroduction: '一步步教你轻松学习自然语言处理基础与实践',
+              imgUrl: require("../../assets/course/demo2.jpg"),
+              keywords: ['机器学习', '自然语言处理'],
+              difficultIdx: '初级',
+              hot: 2314,
+            },{
+              name: '社交类App设计入门',
+              briefIntroduction: '综合案例带你详细透析社区产品交互设计',
+              imgUrl: require("../../assets/course/demo1.jpg"),
+              keywords: ['App', '产品交互'],
+              difficultIdx: '初级',
+              hot: 1290,
+            }, {
+              name: '自然语言处理（NLP）入门与实践',
+              briefIntroduction: '一步步教你轻松学习自然语言处理基础与实践',
+              imgUrl: require("../../assets/course/demo2.jpg"),
+              keywords: ['机器学习', '自然语言处理'],
+              difficultIdx: '初级',
+              hot: 2314,
+            },{
+              name: '社交类App设计入门',
+              briefIntroduction: '综合案例带你详细透析社区产品交互设计',
+              imgUrl: require("../../assets/course/demo1.jpg"),
+              keywords: ['App', '产品交互'],
+              difficultIdx: '初级',
+              hot: 1290,
+            }, {
+              name: '自然语言处理（NLP）入门与实践',
+              briefIntroduction: '一步步教你轻松学习自然语言处理基础与实践',
+              imgUrl: require("../../assets/course/demo2.jpg"),
+              keywords: ['机器学习', '自然语言处理'],
+              difficultIdx: '初级',
+              hot: 2314,
+            },
+          ],
+          currentPage4: 1,
+
         }
     },
     methods: {
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      },
+
 
     }
   }
@@ -116,7 +238,7 @@
                 .rowFour {
                     margin: 15px 0 15px 0;
                     padding-bottom: 15px;
-                    border-bottom: 1px solid #edf1f2;
+                    /*border-bottom: 1px solid #edf1f2;*/
                 }
 
                 button {
@@ -143,6 +265,40 @@
             }
 
 
+        }
+
+        .list-content {
+            width: 80%;
+            margin: 0 auto;
+
+            .isActive {
+                background-color: #ECF5FF;
+                color: #409EFF;
+            }
+
+            .list-card {
+                margin: 15px 0 15px 0;
+
+
+                .image {
+                    width: 100%;
+                }
+            }
+        }
+
+        .footer {
+            background-color: #001528;
+            height: 50px;
+            color: white;
+
+            p {
+                margin-top:4px;
+                margin-bottom: 4px;
+                line-height: 1.8;
+                font-size: 12px;
+                color: #787d82;
+                text-align: center;
+            }
         }
     }
 </style>
